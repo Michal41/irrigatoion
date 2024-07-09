405a74361e69928a8f4e02e24e5c8ddf36368d14
Michal Kanarek <michal.kanarek@gmail.com>
2024-07-09 17:08:23 +0200

    Make logic work with low signal trigered relay

--- backend/services/sensor-service.js
+++ backend/services/sensor-service.js
@@ -2,14 +2,14 @@ const dotenv = require('dotenv');
 dotenv.config();
 const preferenceService = require("../services/preference-service")
 var Gpio = require('onoff').Gpio;
-var PIN = new Gpio(516, 'out');
+var PIN = new Gpio(516, 'out', { initial: 1 }); // on rasberyPI it is physical 7
 
 
 exports.irrigate = async (irrigationTimeInSeconds, sensorName) => {
     console.log("Start Irrigation...")
-    PIN.writeSync(1)
-    await new Promise(resolve => setTimeout(resolve, 1000 * irrigationTimeInSeconds));
     PIN.writeSync(0)
+    await new Promise(resolve => setTimeout(resolve, 1000 * irrigationTimeInSeconds));
+    PIN.writeSync(1)
     PIN.unexport()
     console.log('Irrigation finished')
     return "Success"
--- backend/services/test.js
+++ backend/services/test.js
@@ -1,11 +1,11 @@
 var Gpio = require('onoff').Gpio;
-var PIN = new Gpio(516, 'out'); // on rasberyPI it is physical 7
+var PIN = new Gpio(516, 'out', { initial: 1 }); // on rasberyPI it is physical 7
 
 
 const runPin = async () => {
-  PIN.writeSync(1)
-  await new Promise(resolve => setTimeout(resolve, 10000));
   PIN.writeSync(0)
+  await new Promise(resolve => setTimeout(resolve, 10000));
+  PIN.writeSync(1)
   PIN.unexport()
 }
 
