583ef965c8c7f3948d2db3f60a5f622f1a728706
Michal Kanarek <michal.kanarek@gmail.com>
2024-08-06 18:33:52 +0200

    Create preferences spec

---
+++ backend/spec/preferencesSpec.js
@@ -0,0 +1,17 @@
+const MeasurementService = require("../services/measurement-service");
+const PreferencesService = require("../services/preference-service");
+const Preference = require('../models/preferences');
+
+
+describe("A suite", function() {
+  it("should update user preferences", async function() {
+    // create measurement to create sensor
+    const sensorName = 'fox'
+    const payload =  { minIrrigationIntervalInMinutes: 5,  irrigationTimeInSeconds: 5, capacityBuffer: 5, signalPin: 2, sensorName }
+    await MeasurementService.setMeasurement(50, sensorName)
+    await PreferencesService.getPreference(sensorName)
+    await PreferencesService.updatePreferences(payload, sensorName)
+    const preferences = await Preference.findOne({ sensorName })
+    expect(preferences.irrigationTimeInSeconds).toBe(payload.irrigationTimeInSeconds);
+  });
+});
